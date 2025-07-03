612c68e950ff6ecaf98d89a335aec4597f4a738c
ShantanuNimbalkar <87299589+ShantanuNimbalkar@users.noreply.github.com>
2025-07-03 16:44:52 +0530

    Update Logger.java

--- src/main/java/com/example/Logger.java
+++ src/main/java/com/example/Logger.java
@@ -3,5 +3,6 @@ package com.example;
 public class Logger {
     public void log(String message) {
         System.out.println("LOG: " + message);
+         System.out.println("LOG: " + message);
     }
 }
