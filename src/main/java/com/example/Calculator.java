9b880bc011fb65e8c1cba278cad9c69ae03a4133
shantanunimbalkar <shantanu.nimbalkar@embold.io>
2025-07-17 11:49:44 +0530

    Revert "EMB-Adding a commit"
    This reverts commit 8e65c25c0d630a2c66c2d6600e34ccd553302b72.

---
+++ src/main/java/com/example/Calculator.java
@@ -0,0 +1,28 @@
+package com.example;
+
+public class Calculator {
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int divide(int a, int b) {
+        if (b == 0) throw new IllegalArgumentException("Division by zero");
+        return a / b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int squarePublic(int a) {
+        return square(a);
+    }
+
+    private int square(int a) {
+        return a * a;
+    }
+}
+
