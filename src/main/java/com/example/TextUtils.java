7bb1524dde85c344217a6abcbb05b2d2f2206667
ShantanuNimbalkar <87299589+ShantanuNimbalkar@users.noreply.github.com>
2025-07-21 17:47:48 +0530

    Update TextUtils.java

--- src/main/java/com/example/TextUtils.java
+++ src/main/java/com/example/TextUtils.java
@@ -4,6 +4,11 @@ public class TextUtils {
     public String reverse(String input) {
         int c = 11;
         System.out.println("Hello");
+        try{
+            
+        }catch(Exception e){
+            logger.error("Exception",e);
+        }
         return new StringBuilder(input).reverse().toString();
     }
 }
